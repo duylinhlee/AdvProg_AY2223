7e86b7087984c8865e864256cf099438e9390788
duylinhlee <138662609+duylinhlee@users.noreply.github.com>
2023-07-30 23:25:41 +0700

    L0_L1_L2_L3

--- AdvProg_L1-GuessIt/guessit.cpp
+++ AdvProg_L1-GuessIt/guessit.cpp
@@ -14,7 +14,9 @@ using namespace std;
 ***/
 int generateRandomNumber() {
     // TODO: Return the random number in range 1 to 100
-    return 100;
+    srand(time(0));
+	return rand() % 100 + 1;
+    
 }
 
 
@@ -26,8 +28,10 @@ int generateRandomNumber() {
 ***/
 int getPlayerGuess() {
     // TODO: Ask the player guest and return the player's number
-
-    return 1;
+	int guess;
+	cout << "enter your number: "<<endl;
+	cin >> guess;
+	return guess;
 }
 
 
@@ -46,7 +50,9 @@ string getAnswer(int number, int randomNumber) {
               If number is equal randomNumber, the answer is "Congratulation! You win."
     ***/
     string answer;
-
+	if 		(number > randomNumber) answer = "Your number is higher.";
+	else if (number < randomNumber) answer = "Your number is lower.";
+	else if (number == randomNumber) answer = "Congratulation! You win.";
     return answer;
 }
 
@@ -59,8 +65,8 @@ string getAnswer(int number, int randomNumber) {
 ***/
 bool checkSuccess(string answer) {
     // TODO: return the result after checking that player guessed right or wrong
-    
-    return true;
+    if (answer == "Congratulation! You win.") return true;
+    else return false;
 }
 
 
@@ -73,7 +79,7 @@ bool checkSuccess(string answer) {
 bool checkContinuePlaying(char isContinued) {
     // TODO: return result after checking player continue playing or not
     bool result = false;
-
+	if (isContinued == 'Y') result = true;
     return result;
 }
 
@@ -87,7 +93,8 @@ bool checkContinuePlaying(char isContinued) {
 char getPlayerOpinion() {
     // TODO: Ask the player about continue playing and return the player's choice
     char isContinued;
-
+	cout << "DO YOU WANT TO PLAY AGAIN (Y/N) ?";
+	cin >> isContinued;
     return isContinued;
 }
 
--- AdvProg_L3-HangMan/hangman.cpp
+++ AdvProg_L3-HangMan/hangman.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "hangman.h"
+#include <cctype>
 
 using std::string;
 using std::vector;
@@ -17,7 +18,7 @@ using std::cin;
 int generateRandomNumber(const int min, const int max)
 {
     // TODO: Return a random integer number between min and max
-    return 1;
+    return rand() % (max-min+1) +min;
 }
 
 vector<string> readWordListFromFile(const string& filePath)
@@ -51,7 +52,10 @@ vector<string> readWordListFromFile(const string& filePath)
 bool isCharInWord(const char ch, const string& word)
 {
     // TODO: return true if ch is in word else return false
-    return true;
+    for (char x : word) {
+    	if (ch == x) return true;
+	}
+    return false;
 }
 
 /***
@@ -65,7 +69,13 @@ string chooseWordFromList(const vector<string>& wordList, int index)
 {
     // TODO: Return a lowercase word in the index position of the vector wordList.
     string answer;
-
+	answer = wordList[index];
+	for (int i=0; i<answer.size(); i++)
+	{
+	    if (isupper(answer[i])) {
+	        answer[i] =  tolower(answer[i]);
+	    }
+	}
     return answer;
 }
 
@@ -78,6 +88,9 @@ string chooseWordFromList(const vector<string>& wordList, int index)
 string generateHiddenCharacters(string answerWord){
     // TODO: Based on answerWord's length, generate hidden characters in form of "---"
     string secretWord;
+    for (int i=0; i< secretWord.size(); i++){
+        secretWord += "-";
+    }
 
     return secretWord;
 }
@@ -99,6 +112,10 @@ char getInputCharacter() {
 void updateSecretWord(string& secretWord, const char ch, const string& word)
 {
     // TODO: Update the secret word if the character ch is in the answer word.
+
+    for (int i=0; i<word.size(); i++){
+        if (word[i]==ch) secretWord[i] = ch;
+    }
 }
 
 /***
@@ -110,6 +127,8 @@ void updateSecretWord(string& secretWord, const char ch, const string& word)
 ***/
 void updateEnteredChars(const char ch, string& chars){
     // TODO: append the character ch is in end of the text chars
+
+    chars += ch;
 }
 
 /***
@@ -120,6 +139,7 @@ void updateEnteredChars(const char ch, string& chars){
 ***/
 void updateIncorrectGuess(int& incorrectGuess){
     // TODO: increase the value of incorrectGuess by 1
+    incorrectGuess +=1;
 }
 
 /***
@@ -146,5 +166,17 @@ void processData(const char ch, const string& word,
             update incorrectGuess: call updateIncorrectGuess() function
             update incorrectChars: call updateEnteredChars() function
     ***/
+    bool check;
+    for (char x :word){
+        if (ch == x){
+            updateEnteredChars(ch, correctChars);
+            updateSecretWord (secretWord, ch, word);
+            check = true;
+        }
+    }
+    if (check == false){
+        updateIncorrectGuess(incorrectGuess);
+        updateEnteredChars(ch, correctChars);
+    }
 }
 
